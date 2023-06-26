d0c1c2b5e4f97f4609f02c905b417ee806ab6c2e
NarmathiGopalan <gpn711119104708>
2023-06-26 17:07:16 +0530

    Components

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Function from './Component/Function';
+
+function App() {
+  return (
+    <div className="App">
+         <Function/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Component/ClassComp.jsx
@@ -0,0 +1,12 @@
+import React,{Component} from "react";
+
+class ClassComp extends Component{
+    render(){
+        return(
+            <>
+            <h1>welcome to class component</h1>
+            </>
+        )
+    }
+}
+export default ClassComp;
